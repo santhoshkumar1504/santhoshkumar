cee8d94501a32531e5ad446c35c463359a2ed013
santhoshkumar1504 <santhosh020200@gmail.com>
2025-09-28 12:54:06 +0530

    navbar changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,95 +1,65 @@
+import { useState } from 'react';
 import '../Stylesheets/navStyle.css'
 
 const Navbar = () => {
 
-    function changeMenuBtn() {
-        let menu=document.getElementById('menu');
-        let menusbtn=document.getElementById('menusbtn');
-        let menuscbtn=document.getElementById('menuscbtn');
-
-        menusbtn.style.display="none";
-        menu.style.display="flex";
-        menuscbtn.style.display="flex";
-
-    }
-
- 
-    function closeMenu()
-    {
-        let menu=document.getElementById('menu');
-        let menusbtn=document.getElementById('menusbtn');
-        let menuscbtn=document.getElementById('menuscbtn');
-
-         menusbtn.style.display="flex";
-        menu.style.display="none";
-        menuscbtn.style.display="none";
-    }
- function hideMenu()
- {
-    let menu=document.getElementById('menu');
-        let menusbtn=document.getElementById('menusbtn');
-        let menuscbtn=document.getElementById('menuscbtn');
-
-         menusbtn.style.display="flex";
-        menu.style.display="none";
-        menuscbtn.style.display="none";
- }
+  const [isopen,setOpen]=useState(false);
+  const handler=()=>{
+    setOpen(!isopen);
+  }
 
   return (
-    <div className='navbar navbar-expand-md'>
-        <div className="navbar-brand">Santhosh
+        <div className='sticky-top'>
+      
+        <nav className="navbar navbar-expand-md">
+  <div className="container-fluid">
+    <div>
+        <a className="navbar-brand" href="#home">Santhosh</a>
+    </div>
+   
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={handler}>
+      {isopen ? <i className="bi bi-x-lg text-light p-2"></i>: <i className="bi bi-list text-light p-2"></i> }
+      
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
 
-        <div className='buttons'>
-        {/* Menu Button */}
-        <div className='btn' onClick={changeMenuBtn} id='menusbtn'>
-                <span className='menu-btn'>
-                  <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="white"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
-                </span>
-        </div>
-
-        {/* Close Button */}
-        <div className='btn' >
-            <span className='close-btn' id='menuscbtn' onClick={closeMenu}>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="crimson"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
-                </span>
-        </div>
-        </div>
-
-        
-        </div>
-        
-        {/* Navbar Menu Items */}
-        <div className="contain" id='menu'>
-            <ul className="navbar-nav">
-            <a href="#home" onClick={hideMenu}>
+      <ul className="navbar-nav ms-auto">
+        <a href="#home">
                 <li className="navbar-item">
                     Home
-                </li></a>
-                <a href="#about"  onClick={hideMenu}>
+                </li>
+        </a>
+        <a href="#about">
                 <li className="navbar-item">
-                    About Me
-                </li></a>
-                <a href="#skills"  onClick={hideMenu}>
+                     About Me
+                </li>
+        </a>
+        <a href="#skills">
                 <li className="navbar-item">
                     Skills
-                </li></a>
-                <a href="#projects"  onClick={hideMenu}>
+                </li>
+        </a>
+        <a href="#projects">
                 <li className="navbar-item">
                     Projects
-                </li></a>
-
-                
-                <a href="#certificates"  onClick={hideMenu}>
+                </li>
+        </a>
+        <a href="#certificates">
                 <li className="navbar-item">
-                   Certificates
-                </li></a>
-
-                <a href="#contact"  onClick={hideMenu}>
+                    Certificates
+                </li>
+        </a>
+        <a href="#contact">
                 <li className="navbar-item">
-                    Contact
-                </li></a>
-            </ul>
-        </div>
+                   Contact
+                </li>
+        </a>
+             </ul>
+
+
+    </div>
+</div>
+</nav>
     </div>
   )
 }
